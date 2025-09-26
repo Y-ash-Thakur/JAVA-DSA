970f870779720ad48efaaaa3d9c1131588923228
Y-ash-Thakur <yasht0509@gmail.com>
2025-09-26 16:05:56 +0530

    Q3-Smallest-Char

--- 10-binary search/Code/src/com/yash/Ceiling.java
+++ 10-binary search/Code/src/com/yash/Ceiling.java
@@ -2,17 +2,19 @@ package com.yash;
 
 public class Ceiling {
     public static void main(String[] args) {
-//        int [] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
         int [] arr = {2,3,5,9,14,16,18};
         int target = 15;
-//        int target = 22;
         int ans = ceiling(arr, target);
         System.out.println(ans);
     }
 
-    // return the index
-    // return -1 if it does not exist
+    // return the index of smallest no >= target
     static int ceiling(int[] arr, int target){
+
+        // but what if the target is greater than the gretest number in the array
+        if(target > arr[arr.length - 1]){
+            return -1;
+        }
         int start = 0;
         int end = arr.length - 1;
 
@@ -22,11 +24,11 @@ public class Ceiling {
             int mid = start + (end - start) / 2;
 
             if(target < arr[mid]) {
-//                end = mid - 1;
-                start = mid + 1; // when array is in descending order
+                end = mid - 1;
+//                start = mid + 1; // when array is in descending order
             } else if(target > arr[mid]){
-//                start = mid + 1;
-                end = mid - 1; // when array is in descending order
+                start = mid + 1;
+//                end = mid - 1; // when array is in descending order
             } else {
                 // answer found
                 return mid;
---
+++ 10-binary search/Code/src/com/yash/SmallestLetter.java
@@ -0,0 +1,29 @@
+package com.yash;
+
+public class SmallestLetter {
+    public static void main(String[] args) {
+        char [] arr = {'c','f','j'};
+        char target = 'l';
+        char ans = nextGreatestLetter(arr, target);
+        System.out.println(ans);
+    }
+
+    // return the index of smallest no >= target
+    static char nextGreatestLetter(char[] letters, char target){
+        int start = 0;
+        int end = letters.length - 1;
+
+        while(start <= end){
+            // find the middle element
+//            int mid = (start + end)/2; // might be possible that (start + end) exceeds the range of the integer
+            int mid = start + (end - start) / 2;
+
+            if(target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
