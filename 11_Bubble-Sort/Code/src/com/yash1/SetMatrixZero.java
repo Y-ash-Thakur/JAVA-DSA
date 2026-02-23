88027d2fd31821577658a3252c8e08727aec4215
Y-ash-Thakur <yasht0509@gmail.com>
2026-02-23 10:25:45 +0530

    set matrix 0

---
+++ 11_Bubble-Sort/Code/src/com/yash1/SetMatrixZero.java
@@ -0,0 +1,45 @@
+package com.yash1;
+
+public class SetMatrixZero {
+    public static void main (String [] args){
+        int [][] matrix = {
+                {1,1,1,1},
+                {1,0,1,1},
+                {1,1,0,1},
+                {1,0,0,1},
+        };
+        setZeros(matrix);
+
+        for (int[] row: matrix){
+            for (int val: row){
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void setZeros(int [][] matrix){
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean [] row = new boolean[m];
+        boolean [] col = new boolean[n];
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j ++){
+                if(matrix[i][j] == 0){
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if(row[i] || col[j]){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
