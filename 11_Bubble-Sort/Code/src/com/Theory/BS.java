52ad865efdd70217a221b945714b06175ef23f7e
Y-ash-Thakur <yasht0509@gmail.com>
2026-03-03 23:22:07 +0530

    recursion BS

--- 11_Bubble-Sort/Code/src/com/Theory/BS.java
+++ 11_Bubble-Sort/Code/src/com/Theory/BS.java
@@ -3,7 +3,7 @@ package com.Theory;
 public class BS {
     public static void main(String[] args) {
         int [] arr = {1,2,3,4,55,66,78};
-        int target = 4; 
+        int target = 78;
         System.out.println(search(arr, target, 0, arr.length - 1));
     }
 
